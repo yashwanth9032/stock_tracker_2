c55d7e6d345e448b5a823197f61a93a870f28fbf
Mykl Clason <myklclason@gmail.com>
2016-01-25 04:14:33 +0000

    add user and stock association and display

--- app/assets/javascripts/stocks.js
+++ app/assets/javascripts/stocks.js
@@ -4,6 +4,7 @@ init_stock_lookup = function() {
   $('#stock-lookup-form').on('ajax:before', function(event, data, status) {
     show_spinner();
   });
+
   $('#stock-lookup-form').on('ajax:after', function(event, data, status) {
     hide_spinner();
   });
